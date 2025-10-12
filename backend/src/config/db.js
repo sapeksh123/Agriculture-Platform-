dd0091a37c62b6d43bcaa29396d00fddcb3b51bc
sapeksh <sapekshvishwakarma@gmail.com>
2025-10-12 22:26:27 +0530

    refactor

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(`✅ MongoDB Connected Successfully !!`);
+    console.log(`MongoDB Connected Successfully !! `);
   } catch (error) {
     console.error(`❌ Error: ${error.message}`);
     process.exit(1);
