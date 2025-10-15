70777608eb4b34c01e15bdec38b56f647dbb7a09
sapeksh <sapekshvishwakarma@gmail.com>
2025-10-15 20:18:37 +0530

    create file

---
+++ backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken';
+import User from '../modules/user/userModel.js';
+
+export const protect = async (req, res, next) => {
+  let token = req.headers.authorization?.split(' ')[1];
+  if (!token) return res.status(401).json({ message: 'Not authorised' });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id).select('-password');
+    next();
+  } catch (err) {
+    res.status(401).json({ message: 'Token failed' });
+  }
+};
